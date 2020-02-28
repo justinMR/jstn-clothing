6b60b35642c3fdfbc7c41044dcb0e9128b5b3c0f
Justin Michael Ramirez <JustinMichaelRamirez@gmail.com>
2020-02-28 17:30:49 -0500

    firebase added, sign in page, and sign in with google

---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,24 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+const config = {
+  apiKey: "AIzaSyDyWoisYeEmiNuzI0U-MogzbRwqmaOy61U",
+  authDomain: "jstn-clothing-db-7f0d2.firebaseapp.com",
+  databaseURL: "https://jstn-clothing-db-7f0d2.firebaseio.com",
+  projectId: "jstn-clothing-db-7f0d2",
+  storageBucket: "jstn-clothing-db-7f0d2.appspot.com",
+  messagingSenderId: "79765526079",
+  appId: "1:79765526079:web:5b872762a68eb6343a8a8a"
+};
+
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: "select_account" });
+
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+export default firebase;
