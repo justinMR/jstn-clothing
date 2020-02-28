e1aedadb25028bc3886543b3d21b732417707f42
Justin Michael Ramirez <JustinMichaelRamirez@gmail.com>
2020-02-27 23:19:21 -0500

    added header component

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import "./App.css";
 //imports
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
-
+import Header from "./components/header/header.component";
 const HatsPage = () => (
   <div>
     <h1>hats page</h1>
@@ -15,6 +15,7 @@ const HatsPage = () => (
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage}></Route>
         <Route path='/shop/' component={ShopPage}></Route>
